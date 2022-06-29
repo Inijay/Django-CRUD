9836738b4eff8415a3db8343ee8069a3cac9e18c
Inioluwa Jacob <51286535+Inijay@users.noreply.github.com>
2022-06-29 22:57:06 +0100

    New

--- I4G002143OV2/blog/views.py
+++ I4G002143OV2/blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views import Post
+
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+
+from .models import Post
+
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    fields='_all_'
+    success_url  = reverse_lazy('blog:all')
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '_all_'
+    template_name=["base.html","post_confirm_detele.html","post_details.html","post_forms.html","post_list.html"]
+    
+class PostDetailView(DetailView):
+    model = Post
+    fields='_all_'
+    success_url  = reverse_lazy('blog:all')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields='_all_'
+    success_url  = reverse_lazy('blog:all')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields='_all_'
+    success_url  = reverse_lazy('blog:all')
