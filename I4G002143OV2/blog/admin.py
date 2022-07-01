937808de3948fb98b95abaad928308677c4e0c6b
Inioluwa Jacob <51286535+Inijay@users.noreply.github.com>
2022-07-01 07:13:22 +0100

    New

--- I4G002143OV2/blog/admin.py
+++ I4G002143OV2/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, Title, Slug, Update, Create, Author, Body, Publish, Status
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Title)
+admin.site.register(Slug)
+admin.site.register(Update)
+admin.site.register(Create)
+admin.site.register(Author)
+admin.site.register(Body)
+admin.site.register(Publish)
+admin.site.register(Status)
